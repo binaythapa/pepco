60f2caa82c6e0b7e1cb5418f51d686866d44b7ce
binaythapa <tbinay5@gmail.com>
2024-03-21 15:27:32 +0545

    updating admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+from django.contrib import admin
+
+admin.site.site_header = 'Logic Data Validation'
+
 # Register your models here.
 admin.site.register(File)
 admin.site.register(Project)
